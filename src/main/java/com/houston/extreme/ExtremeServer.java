722c864c8d0b7ce68b2973758d7828e6e433e901
Esko Pylsy <esko.pylsy@houston-inc.com>
2011-10-04 13:39:14 +0300

    Servlet and web.xml

--- src/main/java/com/houston/extreme/ExtremeServer.java
+++ src/main/java/com/houston/extreme/ExtremeServer.java
@@ -2,14 +2,13 @@ package com.houston.extreme;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ExtremeServer {
 
 	@RequestMapping("/")
-	public @ResponseBody String get(@RequestParam String q) {
+	public @ResponseBody String get() {
 		throw new IllegalArgumentException("Not implemented!");
 	}
 }

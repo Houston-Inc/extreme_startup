d9af71842a2aa7ed4eca068cddaa6e61d32a8ae3
Esko Pylsy <esko.pylsy@houston-inc.com>
2011-10-25 20:38:59 +0300

    Initial server implementation

--- src/main/java/com/houston/extreme/ExtremeServer.java
+++ src/main/java/com/houston/extreme/ExtremeServer.java
@@ -2,13 +2,15 @@ package com.houston.extreme;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ExtremeServer {
-
+	
+	// http://localhost:8080/extreme/?q=LOL
 	@RequestMapping("/")
-	public @ResponseBody String get() {
-		throw new IllegalArgumentException("Not implemented!");
+	public @ResponseBody String get(@RequestParam String q) {
+		return "ExtremeStartup" + q;
 	}
 }
